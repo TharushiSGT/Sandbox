40f89d17da5436d07469e5f21f06b7c10412d398
TharushiNE <tharushinethmi.sikuradhipathiganithayalage@my.jcu.edu.au>
2025-09-23 23:24:54 +1000

    Answers for the questions

---
+++ Sandbox/REFLECTION.md.py
@@ -0,0 +1,19 @@
+# CP1404 Practical Reflection
+
+## 1.What percentage of the lecture videos have you watched so far?
+# 80%
+
+## 2.What time, location and duration have you scheduled to watch lecture videos?
+# I've scheduled to watch them on Fridays and Saturdays from 2:00 PM to 4:00 PM when I'm at home. I usually watch for about 30 minutes at a time.
+
+## What are you doing well in the subject so far that you will keep doing?
+# I am doing a good job of starting the practicals early in the week and leaving enough time to ask questions if I get stuck.
+
+## What do you need to stop or change in your work for this subject overall so far?
+# I need to stop multitasking while watching the videos and start taking more detailed notes.
+
+## What are you currently doing poorly for practicals in this subject that you will change?
+# I am not thoroughly reading the instructions before I start coding, which leads to mistakes. I will start by reading the entire practical sheet first.
+
+## What are you doing really well for practicals in this subject?
+# I am actively participating in the practical sessions and collaborating with my classmates to debug problems.
